b690d863a432f5487d475b0665a9dab3baf073a2
Rodney Dev <83191081+rodevrel@users.noreply.github.com>
2021-05-18 10:50:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import time
 
 print("Welcome to my speed quiz/nWhat is your favorite color?")
 q1 = input()
+print("Cool! Now lets get your name")
+q2 = input()
+print("Welcome " + q2)
+time.sleep(2)
+print("Make your move!")
